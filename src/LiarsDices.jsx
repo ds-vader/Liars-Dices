0712da7f991b2ef4b8d4c7bdb7139c7159dc670f
twoSad <dv.medinskiy@gmail.com>
2021-02-21 02:02:31 +0200

    Liars dices v1.1.0 - without an array of cubes, 5 cubes are now displayed when the page loads

--- src/LiarsDices.jsx
+++ src/LiarsDices.jsx
@@ -6,17 +6,16 @@ class LiarsDices extends React.Component {
         super(props);
 
         this.state = {
-            diceCount: 5,
-            dices: []
+            diceCount: 5
         }
     }
 
-    random = () => this.setState({dices: [...new Array(this.state.diceCount)].map(() => Math.floor(Math.random() * 6) + 1)});
+    random = () => {return [...new Array(this.state.diceCount)].map(() => Math.floor(Math.random() * 6) + 1)}
 
     render () {
         return <div className='wrapper'>
             {
-                this.state.dices.map(item => 
+                this.random().map(item => 
                     item === 1 ? <Dice1/> :
                     item === 2 ? <Dice2/> :
                     item === 3 ? <Dice3/> :
@@ -25,10 +24,10 @@ class LiarsDices extends React.Component {
                     item === 6 ? <Dice6/> : <div></div>
                 )
             }
-            <button onClick={() => this.random()}>roll</button>
+            <button onClick={() => this.setState({diceCount: this.state.diceCount})}>roll</button>
             {
-                this.state.diceCount === 1 ? <button onClick={() => this.setState({diceCount: 5, dices: [...new Array(5)].map(() => Math.floor(Math.random() * 6) + 1) })}>go to 5</button> :
-                    <button onClick={() => this.setState({diceCount: this.state.diceCount - 1, dices: this.state.dices.filter((_, i) => i !== 0)})}>-1</button>
+                this.state.diceCount === 1 ? <button onClick={() => this.setState({diceCount: 5})}>go to 5</button> :
+                    <button onClick={() => this.setState({diceCount: this.state.diceCount - 1})}>-1</button>
             }
         </div>
     }
