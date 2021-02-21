355b90a7abad00b620516f295e8ed8f9e93d5c3e
twoSad <dv.medinskiy@gmail.com>
2021-02-21 02:06:26 +0200

    Liars dices v1.2.0 - implemented with hooks

--- src/LiarsDices.jsx
+++ src/LiarsDices.jsx
@@ -1,36 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Dice1, Dice2, Dice3, Dice4, Dice5, Dice6 } from 'react-bootstrap-icons'
 
-class LiarsDices extends React.Component {
-    constructor(props) {
-        super(props);
+const LiarsDices = () => {
+    //const random = () => [...new Array(length)].map(() => Math.floor(Math.random() * 6) + 1);
 
-        this.state = {
-            diceCount: 5
-        }
-    }
+    const [state, setState] = useState({count : 5});
 
-    random = () => {return [...new Array(this.state.diceCount)].map(() => Math.floor(Math.random() * 6) + 1)}
+    const random = () => {return [...new Array(state.count)].map(() => Math.floor(Math.random() * 6) + 1)}
 
-    render () {
-        return <div className='wrapper'>
-            {
-                this.random().map(item => 
+    return (
+        <div className='wrapper'>
+            {random().map(item => 
                     item === 1 ? <Dice1/> :
                     item === 2 ? <Dice2/> :
                     item === 3 ? <Dice3/> :
                     item === 4 ? <Dice4/> :
                     item === 5 ? <Dice5/> :
                     item === 6 ? <Dice6/> : <div></div>
-                )
-            }
-            <button onClick={() => this.setState({diceCount: this.state.diceCount})}>roll</button>
+                )}
+            <button onClick={() => setState(({count}) => ({ count: count}))}>roll</button>
             {
-                this.state.diceCount === 1 ? <button onClick={() => this.setState({diceCount: 5})}>go to 5</button> :
-                    <button onClick={() => this.setState({diceCount: this.state.diceCount - 1})}>-1</button>
+                state.count === 1 ? <button onClick={() => setState(() => ({ count: 5 }))}>go to 5</button> :
+                    <button onClick={() => setState(({count}) => ({ count: count - 1 }))}>-1</button>
             }
         </div>
-    }
+    )
+
 }
 
 export default LiarsDices
